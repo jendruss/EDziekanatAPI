614527e7c93cb0b3d8998c7a5f70e11cbac3b634
jendruss <awalaszczyka@gmail.com>
2021-06-06 14:51:29 +0200

    Dodano DeansOfficeId do zwrotki podczas logowania

--- src/EDziekanat.Application/Users/Dto/UserDto.cs
+++ src/EDziekanat.Application/Users/Dto/UserDto.cs
@@ -1,9 +1,11 @@
-﻿using EDziekanat.Application.Dto;
+﻿using System;
+using EDziekanat.Application.Dto;
 
 namespace EDziekanat.Application.Users.Dto
 {
     public class UserDto : EntityDto
     {
+        public Guid DeansOfficeId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
